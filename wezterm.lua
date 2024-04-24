d1f1ac653b15c4279b5a422fff7e4c1d66f5af2b
Rafael de Bem <rafael.bem@debem.dev>
2024-04-24 20:42:44 -0300

    feat: improve config

--- wezterm.lua
+++ wezterm.lua
@@ -1,27 +1,26 @@
-local wez = require("wezterm")
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
 local config = {}
+local home = os.getenv("HOME")
 
 config.color_scheme = "Catppuccin Mocha"
--- config.font = wez.font("BerkeleyMono Nerd Font Mono", {
--- 	weight = "Bold",
--- 	italic = false,
--- })
 config.enable_tab_bar = false
--- config.window_background_opacity = 1.95
-config.text_background_opacity = 1.0
+config.window_background_opacity = 0.95
+config.text_background_opacity = 0.80
 
 config.window_decorations = "RESIZE"
 
 config.background = {
 	{
 		source = {
-			File = "~/Pictures/Wallpapers/WallPaper 38.jpg",
+			File = home .. "/Pictures/Wallpapers/WallPaper 38.jpg",
 		},
-		hsb = { brightness = 1.00 },
+		hsb = { brightness = 0.01 },
 	},
 }
 
-config.font = wez.font_with_fallback({
+config.font = wezterm.font_with_fallback({
 	{ family = "Berkeley Mono Variable", weight = 550 },
 	"Fira Code",
 	"DengXian",
